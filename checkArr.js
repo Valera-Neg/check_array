7c2443a87f022a39e4e8979c35ef4dfa8e384f56
Valera-Neg <nehovora.val@gmail.com>
2022-11-02 00:15:14 -0400

    first commit

---
+++ checkArr.js
@@ -0,0 +1,20 @@
+/*
+You will be given an array a and a value x. 
+All you need to do is checkwhether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+
+function check(a, x) {
+return a.toString().split(",").find(e => e === x.toString()) ? true : false;  
+}
+
+console.log(check([66, 101], 66)) //> true
+console.log(check([101, 45, 75, 105, 99, 107], 107)) //> true
+console.log(check(['t', 'e', 's', 't'], 'e')) //> true
+console.log(check(['what', 'a', 'great', 'kata'], 'kat')) //> false
+console.log(check([0, 2, 4], 0)) //> true
+
